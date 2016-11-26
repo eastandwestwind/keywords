0a3da1606fe0e4cbc14006af1e79342c28a95179
Catherine Smith <eastandwestwind@gmail.com>
2016-11-26 18:52:06 -0500

    rewrite for serverside only

--- bundle.js
+++ bundle.js
@@ -7168,7 +7168,9 @@ function extend() {
 }
 
 },{}],36:[function(require,module,exports){
-var https = require('https');
+exports.requestStories = function(onStoryListComplete) {
+
+    var https = require('https');
 
 //The url we want is: 'https://hacker-news.firebaseio.com/v0/topstories.json'
     var options = {
@@ -7176,61 +7178,47 @@ var https = require('https');
         path: '/v0/topstories.json'
     };
 
-callback = function(response) {
-    var str = '';
+    callback = function (response) {
+        var str = '';
 
-    //another chunk of data has been recieved, so append it to `str`
-    response.on('data', function (chunk) {
-        str += chunk;
-    });
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
 
-    //the whole response has been recieved, so we just print it out here
-    response.on('end', function () {
-        var json = JSON.parse(str);
-        var storylist = {};
-        for (var i = 0; i < 10 && i < json.length; i++) {
-            var post = json[i];
-            console.log(json[i]);
-            var options = {
-                host: 'hacker-news.firebaseio.com',
-                path: '/v0/item/' + json[i] +'.json'
-            };
-            callback = (function(j){
-                return function(response) {
-                    var story = '';
-                    //another chunk of data has been recieved, so append it to `str`
-                    response.on('data', function (chunk) {
-                        story += chunk;
-                    });
-                    response.on('end', function () {
-                        storylist[j] = story;
-                        console.log(Object.keys(storylist).length);
-                        console.log(storylist);
-                        if (10 == Object.keys(storylist).length) {
-                            for (var s = 0; s < 10; s++) {
-                                story = JSON.parse(storylist[s]);
-                                var storyDiv = document.getElementById("stories");
-                                var listItem = document.createElement('li');
-                                var link = document.createElement('a');
-                                link.href = story['url'];
-                                link.innerText = story['title'];
-                                listItem.appendChild(link);
-                                storyDiv.appendChild(listItem);
-                            }
-                        }
-                    });
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+            var json = JSON.parse(str);
+            var storylist = {};
+            for (var i = 0; i < 10 && i < json.length; i++) {
+                var post = json[i];
+                console.log(json[i]);
+                var options = {
+                    host: 'hacker-news.firebaseio.com',
+                    path: '/v0/item/' + json[i] + '.json'
                 };
-            })(i);
-            https.request(options, callback).end();
-        };
-    });
+                callback = (function (j) {
+                    return function (response) {
+                        var story = '';
+                        //another chunk of data has been recieved, so append it to `str`
+                        response.on('data', function (chunk) {
+                            story += chunk;
+                        });
+                        response.on('end', function () {
+                            storylist[j] = JSON.parse(story);
+                            console.log(Object.keys(storylist).length);
+                            console.log(storylist);
+                            if (10 == Object.keys(storylist).length) {
+                                onStoryListComplete(storylist);
+                            }
+                        });
+                    };
+                })(i);
+                https.request(options, callback).end();
+            }
+            ;
+        });
+    }
+    https.request(options, callback).end();
 }
-
-https.request(options, callback).end();
-
-// var server = http.createServer(function(req, res) {
-//     res.writeHead(200);
-//     res.end('Hello Http');
-// });
-// server.listen(8080);
 },{"https":8}]},{},[36]);
--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 /**
  * Created by Catherine on 11/19/16.
  */
+
 var story = require('./request');
+var http = require('http');
 
-// var storyDiv = document.getElementById("stories");
-// console.log(story.titles.length);
-// for (i = 0; i < story.titles.length; i++){
-//     var listItem = document.createElement('li');
-//     console.log(story.titles[i]);
-//     listItem.appendChild(document.createTextNode(story.titles[i]));
-//     storyDiv.appendChild(listItem);
-// }
-
+var server = http.createServer(function(req, res) {
+    story.requestStories(function(stories){
+        var html = "";
+            for (var s = 0; s < 10; s++) {
+                story = stories[s];
+                html += '<li><a href=' +story['url']+'>'+story['title']+'</li>'
+                // var storyDiv = document.getElementById("stories");
+                // var listItem = document.createElement('li');
+                // var link = document.createElement('a');
+                // link.href = story['url'];
+                // link.innerText = story['title'];
+                // listItem.appendChild(link);
+                // storyDiv.appendChild(listItem);
+            }
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.writeHead(200);
+        res.end(html);
+    });
+});
+server.listen(8080);
