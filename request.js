206777a4c18b9bf6aab149aba678d751f02930b0
Catherine Smith <eastandwestwind@gmail.com>
2016-11-26 17:06:26 -0500

    wait for complete storylist before appending, thus preserving original order

--- request.js
+++ request.js
@@ -34,15 +34,20 @@ callback = function(response) {
                     });
                     response.on('end', function () {
                         storylist[j] = story;
+                        console.log(Object.keys(storylist).length);
                         console.log(storylist);
-                        story = JSON.parse(story);
-                        var storyDiv = document.getElementById("stories");
-                        var listItem = document.createElement('li');
-                        var link = document.createElement('a');
-                        link.href = story['url'];
-                        link.innerText = story['title'];
-                        listItem.appendChild(link);
-                        storyDiv.appendChild(listItem);
+                        if (10 == Object.keys(storylist).length) {
+                            for (var s = 0; s < 10; s++) {
+                                story = JSON.parse(storylist[s]);
+                                var storyDiv = document.getElementById("stories");
+                                var listItem = document.createElement('li');
+                                var link = document.createElement('a');
+                                link.href = story['url'];
+                                link.innerText = story['title'];
+                                listItem.appendChild(link);
+                                storyDiv.appendChild(listItem);
+                            }
+                        }
                     });
                 };
             })(i);
