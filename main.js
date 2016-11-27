8b3307943f43585a3fad05436df84b2386d84178
Catherine Smith <eastandwestwind@gmail.com>
2016-11-27 18:13:38 -0500

    add mustache template

--- main.js
+++ main.js
@@ -2,27 +2,41 @@
  * Created by Catherine on 11/19/16.
  */
 
+var helmet = require('helmet')
+var express = require('express')
+var app = express()
+app.use(helmet())
+
+var mustacheExpress = require('mustache-express');
+
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
 var story = require('./request');
-var http = require('http');
+// var http = require('http');
 var port = process.env.PORT || 8080;
 
-var server = http.createServer(function(req, res) {
+app.get('/',function(req, res) {
     story.requestStories(function(stories){
-        var html = "";
-            for (var s = 0; s < 10; s++) {
-                var story = stories[s];
-                html += '<li><a href=' +story['url']+'>'+story['title']+'</li>'
-                // var storyDiv = document.getElementById("stories");
-                // var listItem = document.createElement('li');
-                // var link = document.createElement('a');
-                // link.href = story['url'];
-                // link.innerText = story['title'];
-                // listItem.appendChild(link);
-                // storyDiv.appendChild(listItem);
-            }
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.writeHead(200);
-        res.end(html);
+        // var html = "";
+        //     for (var s = 0; s < 10; s++) {
+        //         var story = stories[s];
+        //         html += '<li><a href=' +story['url']+'>'+story['title']+'</li>'
+        //         var storyDiv = document.getElementById("stories");
+        //         var listItem = document.createElement('li');
+        //         var link = document.createElement('a');
+        //         link.href = story['url'];
+        //         link.innerText = story['title'];
+        //         listItem.appendChild(link);
+        //         storyDiv.appendChild(listItem);
+        //     }
+        // res.set('Content-Type', 'text/html; charset=utf-8');
+        // res.writeHead(200);
+        res.render('index',{ stories: Object.values(stories) });
     });
 });
-server.listen(port);
+
+app.listen(port);
