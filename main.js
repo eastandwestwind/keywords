bd8890daba2a8d373a97e5bf2be86e2ebb0dcf19
Catherine Smith <eastandwestwind@gmail.com>
2017-09-06 19:06:37 -0400

    adapting to new project

--- main.js
+++ main.js
@@ -2,12 +2,11 @@
  * Created by Catherine on 11/19/16.
  */
 
-"use strict"
-var helmet = require('helmet')
-var express = require('express')
-var app = express()
-app.use(helmet())
-var assert = require('chai').assert
+"use strict";
+var helmet = require('helmet');
+var express = require('express');
+var app = express();
+app.use(helmet());
 var mustacheExpress = require('mustache-express');
 
 // Register '.mustache' extension with The Mustache Express
@@ -16,14 +15,17 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
-var story = require('./request');
+var punctuation = require('./request');
 var port = process.env.PORT || 8080;
 
 app.get('/',function(req, res) {
-    story.requestStories(function(stories){
-        var storyarray = Object.values(stories);
-        res.render('index',{ stories: storyarray });
+    var callArray = ['Next calling Bill dot com this is DJ speaking Help me help you hi I\'m interested in ordering a toilet and you live here in New York and I wanted to know how long it would take to receive ok yeah what toilet are you looking at color twenty three nine four nine fourteen inch ruff and he led me to do ship out today if we want to within the next like hour or so and look at about three three days for transit time so you get him by the end of the week ok what do you take in payment pay pal discover mass card Amex meet amazon payments ok you get to be done that and you want the wax ring with that yeah you can give me the wax ring with that last ring and I have pretty much if you want the toilet seat','hi this is molly welcome to build how can I help you today yes I was looking to return an order'];
+    var keywordArray = ['order','tracking','ordering','payment','ship','shipping','return','looking'];
+    punctuation.requestPunctuatedText(callArray, keywordArray, function(punctuatedArray) {
+        res.render('index',{ originalText: callArray.reverse(), punctuatedText: punctuatedArray.reverse() });
     });
 });
 
 app.listen(port);
+
+
--- request.js
+++ request.js
@@ -1,127 +1,34 @@
-exports.requestStories = function(onStoryListComplete) {
-    "use strict"
-    var https = require('https');
-    var assert = require('chai').assert;
+exports.requestPunctuatedText = function(callArray, keywordArray, onPunctuationComplete) {
+    "use strict";
+    var request = require('request');
 
-// get top story ids
-    var options = {
-        host: 'hacker-news.firebaseio.com',
-        path: '/v0/topstories.json'
-    };
-
-    var callback = function (response) {
-        var str = '';
-
-        //another chunk of data has been recieved, so append it to `str`
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
-
-        //the whole response has been recieved, so we just print it out here
-        response.on('end', function () {
-            var json = JSON.parse(str);
-            var storylist = {};
-            var jsonLength = json.length;
-
-            // Unit Test: always have > 0 stories
-            assert(jsonLength > 0, "jsonLength is 0");
-
-            // account for case where there isn't 10 stories
-            var storiesLength = 10
-            if (jsonLength < 10){
-                storiesLength = jsonLength;
-            };
-
-            // Unit Test: always have <= 10 stories
-            assert(storiesLength <= 10, "storiesLength is greater than 10");
-
-            for (var i = 0; i < storiesLength; i++) {
-                var post = json[i];
-                console.log(post);
-
-                // get top stories
-                var options = {
-                    host: 'hacker-news.firebaseio.com',
-                    path: '/v0/item/' + post + '.json'
-                };
-                var callback = (function (j) {
-                    return function (response) {
-                        var story = '';
-
-                        //another chunk of data has been recieved, so append it to `str`
-                        response.on('data', function (chunk) {
-                            story += chunk;
-                        });
-                        response.on('end', function () {
-                            storylist[j] = JSON.parse(story);
-
-                            // grab top comment for the story if exists
-                            // method fails if no comments or if last story doesn't have comment?
-                            var commentsLength = "";
-
-                            if (storylist[j]['kids']) {
-
-                                commentsLength = Object.keys(storylist[j]['kids']).length;
-                                storylist[j]["commentNum"] = commentsLength;
-
-                                // get comments
-                                var options = {
-                                    host: 'hacker-news.firebaseio.com',
-                                    path: '/v0/item/' + storylist[j]['kids'][0] + '.json'
-                                };
-                                var callback = function (response) {
-                                    var comment = '';
-                                    response.on('data', function (chunk) {
-                                        comment += chunk;
-                                    });
-                                    response.on('end', function () {
-                                        storylist[j]["comments"] = JSON.parse(comment);
-
-                                        // check if we have all stories
-                                        if (storiesLength == Object.keys(storylist).length) {
-                                            console.log("all stories");
-                                            var commentNum = 0;
-
-                                            // loop through keys to check for comments
-                                            for (var k = 0; k < storiesLength; k++) {
-                                                if (storylist[k]["comments"]) {
-                                                    commentNum +=1;
-                                                    console.log(commentNum);
-                                                };
-                                            };
-
-                                            // check if we have as many comments as stories
-                                            if (storiesLength == commentNum){
-                                                // yay, now we send over to main.js
-                                                console.log("all comments");
-
-                                                // loop through keys to trim each URL
-                                                for (var k = 0; k < storiesLength; k++) {
-
-                                                    // ask HN and stuff don't have URLs, so check for it
-                                                    if (storylist[k]["url"]){
-                                                        var url = storylist[k]["url"];
-                                                        var urlNoProtocol = url.replace(/^https?\:\/\//i, "");
-                                                        var urlBase = urlNoProtocol.replace(/(\/\/[^\/]+)?\/.*/, '$1');
-                                                        storylist[k]["urlBase"] = urlBase;
-                                                    }
-                                                };
-                                                onStoryListComplete(storylist);
-                                            }
-                                        };
-                                    })
-                                };
-                                https.request(options, callback).end();
-                            } else {
-                                storylist[j]["comments"] = 'none';
-                                storylist[j]["commentNum"] = 0;
-                            };
-                        });
-                    };
-                })(i);
-                https.request(options, callback).end();
-            };
-        });
+    function donePunctuating() {
+        console.log('all done');
+        console.log(punctuatedArray);
+        onPunctuationComplete(punctuatedArray);
     }
-    https.request(options, callback).end();
-}
+
+    var itemsProcessed = 0;
+    var punctuatedArray = [];
+    var sentencesWithKeywordsArray = [];
+
+    callArray.forEach(function(item, index, array){
+        // Configure the request
+        var options = {
+            url: 'http://bark.phon.ioc.ee/punctuator',
+            method: 'POST',
+            form: {'text': item}
+        };
+
+        // Set the headers and options ...
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                punctuatedArray.push(body);
+                itemsProcessed++;
+                if(itemsProcessed === array.length) {
+                    donePunctuating();
+                }
+            }
+        });
+    });
+};
